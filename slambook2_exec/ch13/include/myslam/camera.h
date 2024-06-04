e0a793b2820501a4fef5a3c09bf364d51662078e
ifreewolf <ifreewolf@163.com>
2024-06-04 19:15:24 +0800

    new note.
    Signed-off-by: ifreewolf <ifreewolf@163.com>

--- slambook2_exec/ch13/include/myslam/camera.h
+++ slambook2_exec/ch13/include/myslam/camera.h
@@ -1,52 +1,52 @@
 #pragma once
-
 #ifndef MYSLAM_CAMERA_H
 #define MYSLAM_CAMERA_H
 
 #include "myslam/common_include.h"
 
-namespace myslam {
+namespace myslam
+{
 
-// Pinhole stereo camera model
-class Camera {
-    public:
-        EIGEN_MAKE_ALIGNED_OPERATOR_NEW;
-        typedef std::shared_ptr<Camera> Ptr;
+// Pinhole stereo camera model  // 针孔双目相机模型
+class Camera
+{
+public:
+    EIGEN_MAKE_ALIGNED_OPERATOR_NEW;
+    typedef std::shared_ptr<Camera> Ptr;
 
-        double fx_ = 0, fy_ = 0, cx_ = 0, cy_ = 0, baseline_ = 0; // camera intrinsics
-        SE3 pose_;      // extrinsic, from stereo camera to single camera
-        SE3 pose_inv_;  // inverse of extrinsics
+    double fx_ = 0, fy_ = 0, cx_ = 0, cy_ = 0, baseline_ = 0;   // camera intrinsics
+    SE3 pose_;      // extrinsic, from stereo camera to single camera
+    SE3 pose_inv_;  // inverse of extrinsics
 
-        Camera();
+    Camera();
 
-        Camera(double fx, double fy, double cx, double cy, double baseline, const SE3 &pose)
-            : fx_(fx), fy_(fy), cx_(cx), cy_(cy), baseline_(baseline), pose_(pose) {
-            pose_inv_ = pose_.inverse();
-        }
+    Camera(double fx, double fy, double cx, double cy, double baseline, const SE3 &pose)
+        : fx_(fx), fy_(fy), cx_(cx), cy_(cy), baseline_(baseline), pose_(pose) {
+        pose_inv_ = pose_.inverse();
+    }
 
-        SE3 pose() const { return pose_; }
-        
-        // return intrinsic matrix
-        Mat33 K() const {
-            Mat33 k;
-            k << fx_, 0, cx_, 0, fy_, cy_, 0, 0, 1;
-            return k;
-        }
+    SE3 pose() const { return pose_; }
 
-        // coordinate transform: world, camera, pixel
-        Vec3 world2camera(const Vec3 &p_w, const SE3 &T_c_w);
+    // return intrinsic matrix
+    Mat33 K() const {
+        Mat33 k;
+        k << fx_, 0, cx_, 0, fy_, cy_, 0, 0 ,1;
+        return k;
+    }
 
-        Vec3 camera2world(const Vec3 &p_3, const SE3 &T_c_w);
+    // coordinate transform: world, camera, pixel
+    Vec3 world2camera(const Vec3 &p_w, const SE3 &T_c_w);
 
-        Vec2 camera2pixel(const Vec3 &p_c);
+    Vec3 camera2world(const Vec3 &p_c, const SE3 &T_w_c);
 
-        Vec3 pixel2camera(const Vec2 &p_p, double depth = 1);
+    Vec2 camera2pixel(const Vec3 &p_c);
 
-        Vec3 pixel2world(const Vec2 &p_p, const SE3 &T_c_w, double depth = 1);
+    Vec3 pixel2camera(const Vec2 &p_p, double depth = 1);
 
-        Vec2 world2pixel(const Vec3 &p_w, const SE3 &T_c_w);
+    Vec3 pixel2world(const Vec2 &p_p, const SE3 &T_w_c, double depth = 1);
+
+    Vec2 world2pixel(const Vec3 &p_w, const SE3 &T_c_w);
+
 };
-
-}
-
-#endif
+}   // namespce myslam
+#endif  // MYSLAM_CAMERA_H
