a9a8dee765374995df4eb75c411f080a58145793
fgs <ifreewolf@163.com>
2024-08-22 23:11:11 +0800

    new note.
    Signed-off-by: fgs <ifreewolf@163.com>

--- slambook2_exec/ch7/pose_estimation_3d2d.cpp
+++ slambook2_exec/ch7/pose_estimation_3d2d.cpp
@@ -41,6 +41,27 @@ void bundleAdjustmentG2O(
 
 int main(int argc, char **argv)
 {
-    
+    if (argc != 5) {
+        std::cout << "usage: pose_estimation_3d2d img1 img2 depth1 depth2" << std::endl;
+        return 1;
+    }
+    // 读取图像
+    Mat img_1 = imread(argv[1], CV_LOAD_IMAGE_COLOR);
+    Mat img_2 = imread(argv[2], CV_LOAD_IMAGE_COLOR);
+    assert(img_1.data && img_2.data && "Can not load images!");
+
+    vector<KeyPoint> keypoints_1, keypoints_2;
+    vector<DMatch> matches;
+    find_feature_matches(img_1, img_2, keypoints_1, keypoints_2, matches);
+    std::cout << "一共找到了" << matches.size() << "组匹配点" << std::endl;
+
+    // 建立3D点
+    Mat d1 = imread(argv[3], CV_LOAD_IMAGE_UNCHANGED);  // 深度图为16位无符号数，单通道图像
     return 0;
 }
+
+/*
+https://blog.csdn.net/u011341856/article/details/107758182
+
+超定方程A x = 0 \bm A\bm x =0Ax=0的解，就是对A \bm AA进行SVD分解之后的最小特征值对应的特征向量。
+*/
