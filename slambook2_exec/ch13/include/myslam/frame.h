8dee5cb49c9ccf88f0ed4903c3096a7d8df9de0c
fgs <ifreewolf@163.com>
2024-08-29 23:37:21 +0800

    start slam architech

--- slambook2_exec/ch13/include/myslam/frame.h
+++ slambook2_exec/ch13/include/myslam/frame.h
@@ -6,59 +6,57 @@
 #include "myslam/camera.h"
 #include "myslam/common_include.h"
 
-namespace myslam
-{
+namespace myslam {
 
-// forward declare
+// forward declare 前向声明
 struct MapPoint;
 struct Feature;
 
 /**
  * 帧
- * 每一帧分配独立id，关键帧分配关键ID
+ * 每一帧分配独立id，关键帧分配关键帧ID
 */
-struct Frame
-{
-public:
-    EIGEN_MAKE_ALIGNED_OPERATOR_NEW;
-    typedef std::shared_ptr<Frame> Ptr;
+struct Frame {
+    public:
+        EIGEN_MAKE_ALIGNED_OPERATOR_NEW; // 用于在Eigen C++库中启用对齐内存分配。
+        typedef std::shared_ptr<Frame> Ptr;
 
-    unsigned long id_ = 0;          // id of this frame
-    unsigned long keyframe_id_ = 0; // id of key frame
-    bool is_keyframe_ = false;      // 是否为关键帧
-    double time_stamp_;             // 时间戳，暂不使用
-    SE3 pose_;                      // Tcw 形式Pose
-    std::mutex pose_mutex_;         // Pose 数据锁
-    cv::Mat left_img_, right_img_;  // stereo images
+        unsigned long id_ = 0;          // id of this frame
+        unsigned long keyframe_id_ = 0; // id of key frame
+        bool is_keyframe_ = false;      // 是否为关键帧
+        double time_stamp_;             // 时间戳，暂不使用
+        SE3 pose_;                      // Tcw形式pose
+        std::mutex pose_mutex_;         // Pose数据锁，位姿这个数据会被前后端同时设置或访问，所以需要给函数访问添加锁
+        cv::Mat left_img_, right_img_;  // stereo images 
 
-    // extracted features in left images
-    std::vector<std::shared_ptr<Feature>> features_left_;
-    // corresponding features in right image, set to nullptr if no corresponding
-    std::vector<std::shared_ptr<Feature>> features_right_;
+        // 左图提取的特征，与之对应的右图特征，如果右图特征没有，则置为nullptr
+        // extracted features in left image
+        std::vector<std::shared_ptr<Feature>> features_left_;
+        // corresponding features in right image, set to nullptr if no corresponding
+        std::vector<std::shared_ptr<Feature>> features_right_;
 
-public: // data members
-    Frame() {}
+    public: // data members
+        Frame() {} // 无参构造
+        Frame(long id, double time_stamp, const SE3 &pose, const Mat &left, const Mat &right); // 有参构造
 
-    Frame(long id, double time_stamp, const SE3 &pose, const Mat &left, const Mat &right);
+        // set and get pose, thread safe
+        SE3 Pose() {
+            std::unique_lock<std::mutex> lck(pose_mutex_);
+            return pose_;
+        }
 
-    // set and get post, thread safe
-    SE3 Pose() {
-        std::unique_ptr<std::mutex> lck(pose_mutex_);
-        return pose_;
-    }
+        void SetPose(const SE3 &pose) {
+            std::unique_lock<std::mutex> lck(pose_mutex_);
+            pose_ = pose;
+        }
 
-    void SetPose(const SE3 &pose) {
-        std::unique_ptr<std::mutex> lck(pose_mutex_);
-        pose_ = pose;
-    }
+        // 设置关键帧并分配关键帧id
+        void SetKeyFrame();
 
-    // 设置关键帧并分配关键帧id
-    void SetKeyFrame();
-
-    // 工厂构建模式，分配id
-    static std::shared_ptr<Frame> CreateFrame();
+        // 工厂模式，分配id
+        static std::shared_ptr<Frame> CreateFrame(); // 用于构建Frame，外部构建Frame的接口
 };
 
-}   // namespace myslam
+}
 
-#endif  // MYSLAM_FRAME_H
+#endif
--- slambook2_exec/ch13/include/myslam/mappoint.h
+++ slambook2_exec/ch13/include/myslam/mappoint.h
@@ -2,10 +2,9 @@
 #ifndef MYSLAM_MAPPOINT_H
 #define MYSLAM_MAPPOINT_H
 
-#include "myslam/common_include.h"
+namespace myslam {
 
-namespace myslam
-{
+// forward declare
 struct Frame;
 struct Feature;
 
@@ -13,49 +12,18 @@ struct Feature;
  * 路标点类
  * 特征点在三角化之后形成路标点
 */
-
-struct MapPoint
-{
+struct MapPoint {
 public:
     EIGEN_MAKE_ALIGNED_OPERATOR_NEW;
     typedef std::shared_ptr<MapPoint> Ptr;
-    unsigned long id_ = 0;      // ID
+
+    unsigned long id_ = 0;      // id
     bool is_outlier_ = false;
     Vec3 pos_ = Vec3::Zero();   // Position in world
     std::mutex data_mutex_;
-    int observed_times_ = 0;    // being ovserved by feature matching algo.
+    int observed_times_ = 0;    // being ovserved by feature matching algo
     std::list<std::weak_ptr<Feature>> observations_;
-
-    MapPoint() {}
-    
-    MapPoint(long id, Vec3 position);
-
-    Vec3 Pos() {
-        std::unique_lock<std::mutex> lck(data_mutex_);
-        return pos_;
-    }
-
-    void SetPos(const Vec3 &pos) {
-        std::unique_ptr<std::mutex> lck(data_mutex_);
-        pos_ = pos;
-    }
-
-    void AddObservation(std::shared_ptr<Feature> feature) {
-        std::unique_lock<std::mutex> lck(data_mutex_);
-        observations_.push_back(feature);
-        observed_times_++;
-    }
-
-    void RemoveObservation(std::shared_ptr<Feature> feat);
-
-    std::list<std::weak_ptr<Feature>> GetObs() {
-        std::unique_ptr<std::mutex> lck(data_mutex_);
-        return observations_;
-    }
-
-    // factory function
-    static MapPoint::Ptr CreateNewMappoint();
+};
 }
 
-}   // namespace myslam
-#endif  // MYSLAM_MAPPOINT_H
+#endif
