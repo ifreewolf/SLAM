2e5489606d08fa35fd06a8b517ed6128dca1e31b
fgs <ifreewolf@163.com>
2024-09-08 23:32:45 +0800

    new note.
    Signed-off-by: fgs <ifreewolf@163.com>

--- slambook2_exec/ch13/src/frontend.cpp
+++ slambook2_exec/ch13/src/frontend.cpp
@@ -19,13 +19,17 @@ Frontend::Frontend() {
 bool Frontend::AddFrame(myslam::Frame::Ptr frame) {
     current_frame_ = frame;
 
-    switch (status_)
+    switch (status_) // status_是前端Frontend的类成员，所以这里判断的是整个前端的状态
     {
     case FrontendStatus::INITING:
-        StereoInit();
+        StereoInit(); // bool函数,用来做初始化
         break;
     case FrontendStatus::TRACKING_GOOD:
     case FrontendStatus::TRACKING_BAD:
+        // 此处的TRACKING_GOOD和TRACKING_BAD并非跟踪失败或者跟踪成功的含义，这里只是跟踪条件好坏的区别
+        // 当跟踪的特征点数目充足时就是good，当特征点数目不足时就是bad，但不管条件恶劣与否，并不涉及失败的问题
+        // 当特征点少到不行时，就已经不是good和bad的问题，太少的时候就认为跟踪丢了，设置为LOST，这个LOST可以理解为失败。
+        // 所以，LOST之后需要reset系统
         Track();
         break;
     case FrontendStatus::LOST:
@@ -35,6 +39,31 @@ bool Frontend::AddFrame(myslam::Frame::Ptr frame) {
 
     last_frame_ = current_frame_;
     return true;
+}
+
+// 根据上面的Addframe函数，应当在后续实现StereoInit，Track和Reset各个函数
+bool Frontend::StereoInit()
+{
+    int num_features_left = DetectFeatures(); // 一个frame同时包含左右目的图像，这一步是在提取左目特征
+    // 通常在左目当中提取特征时特征点数量是一定能保证的。
+    int num_coor_features = FindFeaturesInRight(); // 根据左目特征在右目中找对应，虽然左目提取时特征点数量能够保证，但匹配过程无法确保能够在右目图像中为所有的左目特征找到对应，所以这一步最后找到的对应特征数目不一定满足初始化条件。
+    if (num_coor_features < num_features_init_) { // 对应数目不足，无法初始化
+        return false;
+    }
+
+    bool build_map_success = BuildInitMap(); // 初始化成功，则开始建立初始的地图
+    if (build_map_success) { // 初始地图建立成功
+        status_ = FrontendStatus::TRACKING_GOOD; // 前端状态可有INITING转变为TRACKING_GOOD
+        if (viewer_) { // 可视化是否开启
+            viewer_->AddCurrentFrame(current_frame_); // 将当前帧加入可视化器
+            viewer_->UpdateMap(); // 更新可视化内容以显示当前帧
+
+            // 为什么Track()函数里面只有AddCurrentFrame()
+        }
+        return true;
+    }
+    // 如果初始化地图没有建立成功，则会跳过上述if段落，则还是没能完成初始化，返回false
+    return false;
 }
 
 
